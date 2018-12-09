10587c1d05d45ce79d683b8aae9f147c6483cf3c
kevinhayesanderson1995 <kevinhayesanderson@gmail.com>
2018-12-09 19:07:58 +0530

    First Migration

---
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly List<Customer> _customers = new List<Customer>()
+        {
+            new Customer(){Name = "Kevin",Id = 1},
+            new Customer(){Name = "Hayes",Id = 2},
+            new Customer(){Name = "Anderson",Id = 3},
+        };
+        // GET: Customer
+        public ActionResult Index()
+        {
+            
+            return View(_customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_customers[id-1]);
+        }
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly List<Movie> _movies=new List<Movie>()
+        {
+            new Movie(){Id = 1,Name="Shrek"},
+            new Movie(){Id = 2,Name="Atlas"},
+            new Movie(){Id = 3,Name="Wanted"}
+
+        };
+        // GET: Movies
+        public ActionResult Index()
+        {
+           return View(_movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_movies[id - 1]);
+        }
+        
+    }
+}
